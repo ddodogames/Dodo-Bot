a4416aa2c9214931bf97531f08259874e760a143
ddodogames <69091682+ddodogames@users.noreply.github.com>
2025-04-29 00:03:48 +0300

    update avatar slash to be same as prefix

--- commands/Slash/avatar.js
+++ commands/Slash/avatar.js
@@ -7,7 +7,7 @@ info: {
 },
 code: `$interactionReply[{newEmbed:{title:$get[username]'s Avatar}{image:$userAvatar[$get[user]]}{color:$getVar[embedcolor]}}
 
-{actionRow:{button:Download:5:$nonEscape[$userAvatar[$get[user]]]:false}};all;true]
+{actionRow:{button:PNG:5:$nonEscape[$userAvatar[$get[user];4096;true;png]]:false}{button:JPG:5:$nonEscape[$userAvatar[$get[user];4096;true;jpg]]:false}{button:WEBP:5:$nonEscape[$userAvatar[$get[user];4096;true;webp]]:false}};all;true]
 
 $let[username;$advancedReplaceText[$checkCondition[$hasUserTag[$get[user]]==false];true;$username[$get[user]];false;$userTag[$get[user]]]]
 $let[user;$advancedReplaceText[$checkCondition[$slashOption[user]==];true;$authorID;false;$slashOption[user]]]
--- handlers/variables.js
+++ handlers/variables.js
@@ -15,7 +15,7 @@ module.exports = {
   versionCodename: "Shiny cookies",
   libraryversiondevcheck: "off",
   buildStatus: "Incomplete",
-  buildDate: "1745696073844",
+  buildDate: "1745874202302",
   buildBranch: "canary",
   buildRevision: "0",
   showbuildinfo: "on",
