4c8dbee9577ca9e3f8a56529b93e8c4f2b3440ad
ddodogames <69091682+ddodogames@users.noreply.github.com>
2025-05-03 02:39:44 +0300

    nothing much

--- commands/Automation/Awaited/levelingresets.js
+++ commands/Automation/Awaited/levelingresets.js
@@ -13,7 +13,11 @@ module.exports = [{
     $resetUserVar[xpLimit;$guildID]
     $resetUserVar[previouslevel;$guildID]
     $setGuildVar[islevelingreset;yes]
-    `},{
+    `}]
+
+   /* In a future version!
+
+    {
     name: "forEachMemberID",
     type: "awaited",
     code: `
@@ -21,4 +25,5 @@ module.exports = [{
     $deleteVar[previouslevel;{value}_$guildID;main]
     $deleteVar[xp;{value}_$guildID;main]
     $deleteVar[xpLimit;{value}_$guildID;main]`
-    }]
+    }
+    */
--- handlers/variables.js
+++ handlers/variables.js
@@ -15,7 +15,7 @@ module.exports = {
   versionCodename: "Shiny cookies",
   libraryversiondevcheck: "off",
   buildStatus: "Incomplete",
-  buildDate: "1745874202302",
+  buildDate: "1746229169332",
   buildBranch: "canary",
   buildRevision: "0",
   showbuildinfo: "on",
