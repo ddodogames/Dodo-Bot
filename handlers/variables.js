53ed5c5cfbaf42ec20c4b9ca0e2173c3036c102b
ddodogames <69091682+ddodogames@users.noreply.github.com>
2025-05-16 21:53:09 +0300

    bump to fix bugs

--- handlers/variables.js
+++ handlers/variables.js
@@ -15,7 +15,7 @@ module.exports = {
   versionCodename: "Shiny cookies",
   libraryversiondevcheck: "off",
   buildStatus: "Incomplete",
-  buildDate: "1747420006867",
+  buildDate: "1747421573546",
   buildBranch: "canary",
   buildRevision: "0",
   showbuildinfo: "on",
