6c7a2943d6b6ac9a820757986a92d1e84c0b6206
ddodogames <69091682+ddodogames@users.noreply.github.com>
2025-08-06 20:05:09 +0300

    Forgot to add this

--- handlers/variables.js
+++ handlers/variables.js
@@ -15,7 +15,7 @@ module.exports = {
   versionCodename: "Metal box",
   libraryversiondevcheck: "off",
   buildStatus: "Incomplete",
-  buildDate: "1754499612049",
+  buildDate: "1754499898215",
   buildBranch: "canary",
   buildRevision: "0",
   showbuildinfo: "on",
