b346ef2d8d702943ce7e5cc317b7fc2e40638726
ddodogames <69091682+ddodogames@users.noreply.github.com>
2026-02-04 21:29:00 +0200

    Add support for GIF banners

--- commands/Automation/Interactions/Buttons/version-interactions.js
+++ commands/Automation/Interactions/Buttons/version-interactions.js
@@ -10,7 +10,7 @@ $ephemeral
 $interactionUpdate[
 $title[Changes]
 $description[
-*No changes have been added yet*
+* Display banners as GIF if detected in \`banner\` command
 ]
 $if[$getGlobalVar[pre_release]==on;
 $attachment[./assets/warning.png;warning.png]
