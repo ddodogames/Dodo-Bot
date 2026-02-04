261a1f532ed390de937c04b0ea39674fc212a5d3
ddodogames <69091682+ddodogames@users.noreply.github.com>
2026-02-04 21:30:43 +0200

    No need for useless `;` if unused

--- commands/Misc/banner.js
+++ commands/Misc/banner.js
@@ -17,7 +17,7 @@ $let[error;$advancedReplace[$checkCondition[$get[user]==$authorID];true;You don'
 $onlyIf[$userBanner[$get[user]]!=;$get[error]]
 
 $title[$get[username]'s banner]
-$image[$userBanner[$get[user];;]]
+$image[$userBanner[$get[user]]]
 $color[$getGlobalVar[embedcolor]]
 $addActionRow
 $addButton[$userBanner[$get[user];;png];Open in browser;Link]
