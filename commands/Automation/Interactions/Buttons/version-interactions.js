a95bf0cd07ab544e595c1cbf0d2877ef06322e5a
ddodogames <69091682+ddodogames@users.noreply.github.com>
2026-02-10 14:16:50 +0200

    Finalize changelog

--- commands/Automation/Interactions/Buttons/version-interactions.js
+++ commands/Automation/Interactions/Buttons/version-interactions.js
@@ -11,6 +11,7 @@ $interactionUpdate[
 $title[Changes]
 $description[
 * Re-added support for GIF banners in \`banner\` command from v2
+* Added 2 new commands called \`greyscale\` and \`showerthoughts\`
 ]
 $if[$getGlobalVar[pre_release]==on;
 $attachment[./assets/warning.png;warning.png]
@@ -37,10 +38,11 @@ $ephemeral
 $interactionUpdate[
 $title[Bug Fixes]
 $description[
-* Fixed "Last updated on" text being displayed for released pre-releases builds like Alpha 0 in \`version\` command
 * (Devs only) Fixed outdated description of "Show build info" option seen in \`dev-panel\` command
 * Require \`EmbedLinks\` as a permission so that commands showing embed work properly
   * It has been listed in \`perms\` command as a result of this
+* (Pre-release only) Fixed how pre-releases are made by properly following $hyperlink[Semantic Versioning specification;https://semver.org/]
+  * This also makes version string in source code less confusing
 ]
 $if[$getGlobalVar[pre_release]==on;
 $attachment[./assets/warning.png;warning.png]
@@ -71,6 +73,9 @@ $description[
   * Build numbers will also no longer show up for released builds
 * Completely remove version revisions in favor of small hotfix releases
 * Slightly change the description of "Why this exists?" button seen in \`perms\` command
+  * Also shorten the description of \`about\` command
+* (Source code) Slight changes to README such as replacing aoi EOL mentions with the sudden revival
+* (Source code) Bumped ForgeScript and \`@dotenvx/dotenvx\` to their latest version
 ]
 $if[$getGlobalVar[pre_release]==on;
 $attachment[./assets/warning.png;warning.png]
