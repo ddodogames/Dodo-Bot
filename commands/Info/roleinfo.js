39a2afe123c49be6df357b2550374eaeda9f3937
ddodogames <69091682+ddodogames@users.noreply.github.com>
2026-02-13 21:32:16 +0200

    Added error messages for `roleinfo`

--- commands/Info/roleinfo.js
+++ commands/Info/roleinfo.js
@@ -3,11 +3,18 @@ module.exports = {
     type: "messageCreate",
     info: {
         description: "Returns information about the specified role.",
+        usage: "`roleinfo <role>`",
         perms: ["`SendMessages`", "`EmbedLinks`"]
     },
     aliases: ["role", "ri"],
     code: `$userCooldown[roleinfocmd;3s;Cooldown has been triggered! Please, wait!
     Time remaining: <t:$trunc[$divide[$sum[$getTimestamp;$getUserCooldownTime[roleinfocmd]];1000]]:R>]
+
+$let[role;$findRole[$guildID;$message]]
+
+$onlyIf[$get[role]!=;Please mention a role to view it's information.]
+
+$onlyIf[$roleExists[$guildID;$get[role]]==true;Please mention a valid role that exists in this server.]
 
  Not done yet.
 `
