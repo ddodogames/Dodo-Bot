16ef45593d060bafeb3ad98dc442e41bad5b9d8d
ddodogames <69091682+ddodogames@users.noreply.github.com>
2026-03-22 10:15:00 +0200

    Change the note to a solution

--- commands/Info/channelinfo.js
+++ commands/Info/channelinfo.js
@@ -14,7 +14,7 @@ $let[servericon;$advancedReplace[$checkCondition[$guildIcon==];true;$userDefault
 $let[channel;$findChannel[$message;false]]
 
 $onlyIf[$get[channel]!=;Please specify a channel (via mention, id or name) to view it's information.
--# Threads are supported but some of them may not work properly.
+-# For Private Threads to work, ensure i'm a member in them.
 ]
 
 $onlyIf[$guildChannelExists[$guildID;$get[channel]]==true;Please mention a valid channel that exists in this server.]
