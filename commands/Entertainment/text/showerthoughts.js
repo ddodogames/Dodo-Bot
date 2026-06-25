259362461e5421a46d7bb231e8f872d1da114c08
ddodogames <69091682+ddodogames@users.noreply.github.com>
2026-06-25 23:59:42 +0300

    Disable showerthoughts for now

--- commands/Entertainment/text/showerthoughts.js
+++ commands/Entertainment/text/showerthoughts.js
@@ -7,6 +7,8 @@ module.exports = {
     type: "messageCreate",
     code: `$userCooldown[showerthoughtscmd;3s;Cooldown has been triggered! Please wait!
     Time remaining: <t:$trunc[$divide[$sum[$getTimestamp;$getUserCooldownTime[showerthoughtscmd]];1000]]:R>]
+    $onlyIf[disabled!=disabled;This command has been disabled until further notice due to issues.]
+
     $let[status;$httpRequest[https://api.popcat.xyz/v2/showerthoughts;get]]
     $onlyIf[$get[status]==200;Unable to fetch data for Shower Thoughts. Please try again later.]
     $author[Shower Thoughts]
