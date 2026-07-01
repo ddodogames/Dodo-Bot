ebd4316469ce98b2c1d3c566749271024c625309
ddodogames <69091682+ddodogames@users.noreply.github.com>
2026-07-01 04:42:38 +0300

    bump fs dev + dotenvx updated to v2

--- commands/Automation/Interactions/Buttons/version-interactions.js
+++ commands/Automation/Interactions/Buttons/version-interactions.js
@@ -68,7 +68,7 @@ $ephemeral
 $interactionUpdate[
 $title[Other]
 $description[
-* (Source code) Bumped \`@dotenvx/dotenvx\` package to \`v1.75.1\`
+* (Source code) Bumped \`@dotenvx/dotenvx\` package to \`v2.0.0\`
 * The progressbar in \`rank\` cmd has been slightly updated to sometimes include \`#\` at some points
 * Include a link in \`canary\` for users who want to run the latest Canary build on their bots
 * (Source code) Temporarily update \`discord-gamecord\` to DeusDrizzyy's fork to test stuff (which includes changes to some cmds here)
